3bde81cd38ced7cb180db9d11ca31c181ed330cd
Abdul Marzouk <me.abdulmarzouk@gmail.com>
2022-05-20 17:30:20 +0530

    javascript task

---
+++ task.js
@@ -0,0 +1,15 @@
+let names = [
+    "marzouk",
+    "karan",
+    "niyas",
+    "thariq",
+    "rashed",
+    "niyas",
+    "marzouk",
+  ];
+  
+
+  let unique_names = [new Set(names)];
+  
+  
+  console.log(unique_names);
